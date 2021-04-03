e190b3fbd87d743a232395e7f43ce2fab2656a95
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-03 21:09:22 +0300

    Added memoization of filtered items selector.

--- src/redux/contacts-selectors.js
+++ src/redux/contacts-selectors.js
@@ -1,17 +1,18 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 const getFilterValue = state => state.contacts.filter;
 
 const getAllContacts = state => state.contacts.items;
 
-const getFilteredItems = state => {
-  const filter = getFilterValue(state);
-  const items = getAllContacts(state);
-
-  return items.filter(
-    ({ name, number }) =>
-      name.toLowerCase().includes(filter.toLocaleLowerCase()) ||
-      number.includes(filter),
-  );
-};
+const getFilteredItems = createSelector(
+  [getAllContacts, getFilterValue],
+  (items, filter) =>
+    items.filter(
+      ({ name, number }) =>
+        name.toLowerCase().includes(filter.toLocaleLowerCase()) ||
+        number.includes(filter),
+    ),
+);
 
 const getLoading = state => state.contacts.loading;
 
