1a3db91d65b1a68d79a5182ec848338800a39ba8
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-04-03 14:18:39 +0300

    Added fetch initial contacts in ContactsList. Added Loader.

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,49 +1,70 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
-import { deleteContact } from '../../redux/contacts-operations';
+import { fetchContacts, deleteContact } from '../../redux/contacts-operations';
 
 import styles from './ContactList.module.css';
+// class  extends Component {
+//   state = {  }
+//   render() {
+//     return (  );
+//   }
+// }
 
-const ContactList = ({ filtered, onDeleteContact }) => {
-  return (
-    <ul className={styles.contacts}>
-      {filtered.length ? (
-        filtered.map(contact => (
-          <li key={contact.id} className={styles.item}>
-            <div>
-              <p>{contact.name}:</p>
-              <p>{contact.number}</p>
-            </div>
+// export default ;
 
-            <button
-              className={styles.btn}
-              onClick={() => {
-                onDeleteContact(contact.id);
-              }}
-            >
-              Delete
-            </button>
-          </li>
-        ))
-      ) : (
-        <li className={styles.notification}>No contact found.</li>
-      )}
-    </ul>
-  );
-};
+class ContactList extends Component {
+  state = {};
 
-ContactList.propTypes = {
-  filtered: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    }).isRequired,
-  ).isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
-};
+  componentDidMount() {
+    this.props.fetchContacts();
+  }
+
+  render() {
+    const { filtered, onDeleteContact, isLoading } = this.props;
+
+    return (
+      <>
+        {isLoading && <p>Loading...</p>}
+        <ul className={styles.contacts}>
+          {filtered.length ? (
+            filtered.map(contact => (
+              <li key={contact.id} className={styles.item}>
+                <div>
+                  <p>{contact.name}:</p>
+                  <p>{contact.number}</p>
+                </div>
+
+                <button
+                  className={styles.btn}
+                  onClick={() => {
+                    onDeleteContact(contact.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            ))
+          ) : (
+            <li className={styles.notification}>No contact found.</li>
+          )}
+        </ul>
+      </>
+    );
+  }
+}
+
+// ContactList.propTypes = {
+//   filtered: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.number.isRequired,
+//       name: PropTypes.string.isRequired,
+//       number: PropTypes.string.isRequired,
+//     }).isRequired,
+//   ).isRequired,
+//   onDeleteContact: PropTypes.func.isRequired,
+// };
 
 const mapStateToProps = state => {
   const filtered = state.contacts.items.filter(
@@ -53,11 +74,13 @@ const mapStateToProps = state => {
   );
   return {
     filtered,
+    isLoading: state.contacts.loading,
   };
 };
 
 const mapDispatchToProps = dispatch => ({
   onDeleteContact: contactId => dispatch(deleteContact(contactId)),
+  fetchContacts: () => dispatch(fetchContacts()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
--- src/redux/contacts-reducers.js
+++ src/redux/contacts-reducers.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import { createReducer } from '@reduxjs/toolkit';
 import {
+  fetchContactRequest,
+  fetchContactSuccess,
+  fetchContactError,
   addContactRequest,
   addContactSuccess,
   addContactError,
@@ -11,6 +14,7 @@ import {
 } from './contacts-actions';
 
 const items = createReducer([], {
+  [fetchContactSuccess]: (_, { payload }) => payload,
   [addContactSuccess]: (state, { payload }) => [payload, ...state],
   [deleteContactSuccess]: (state, { payload }) =>
     state.filter(contact => contact.id !== payload),
@@ -21,6 +25,9 @@ const filter = createReducer('', {
 });
 
 const loading = createReducer(false, {
+  [fetchContactRequest]: () => true,
+  [fetchContactSuccess]: () => false,
+  [fetchContactError]: () => false,
   [addContactRequest]: () => true,
   [addContactSuccess]: () => false,
   [addContactError]: () => false,
